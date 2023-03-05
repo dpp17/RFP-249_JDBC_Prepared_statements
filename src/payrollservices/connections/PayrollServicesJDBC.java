6b35fdfb07917508a6040542718cec44e900e885
dpp17 <durga123q.com>
2023-03-06 05:04:52 +0530

    Added:payroll services jdbc

--- src/payrollservices/connections/PayrollServicesJDBC.java
+++ src/payrollservices/connections/PayrollServicesJDBC.java
@@ -2,30 +2,51 @@ package payrollservices.connections;
 
 import java.sql.*;
 
-import payrollservices.controller.EmployeePayroll;
-import payrollservices.model.EmployeePayrollPojo;
+//import payrollservices.controller.EmployeePayroll;
+//import payrollservices.model.EmployeePayrollPojo;
 
 public class PayrollServicesJDBC {
 
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
-		EmployeePayroll employeeObj = new EmployeePayroll();
-		EmployeePayrollPojo employeePojo = employeeObj.getDetails();
+//		EmployeePayroll employeeObj = new EmployeePayroll();
+//		EmployeePayrollPojo employeePojo = employeeObj.getDetails();
 		
 		Class.forName("com.mysql.jdbc.Driver");
 		final String url = "jdbc:mysql://localhost:3306/payroll_service";
 		final String userName = "root";
 		final String password = "Lambop@12345";
 		Connection connection = DriverManager.getConnection(url,userName,password);
-		String query = "update employee_payroll set NetPay = ? where EmployeeName = ?;";
+		String query = "select * from employee_payroll where StartDate between cast('2015-01-01' as date) and date(now()); ";
 		
-		//Statement statement = connection.createStatement();
-		PreparedStatement statement = connection.prepareStatement(query);
-		statement.setInt(1, employeePojo.getNetPay());
-		statement.setString(2, employeePojo.getEmployeeName());
+		Statement statement = connection.createStatement();
 		
-
-		int res = statement.executeUpdate();
-		System.out.println(res+" :: records inserted");
+		ResultSet resultSet = statement.executeQuery(query);
+		while(resultSet.next()) {
+			//int employeeID = resultSet.getInt("EmployeeID");
+			String employeeName = resultSet.getString("EmployeeName");
+			int netPay = resultSet.getInt("NetPay");
+			String startDate = resultSet.getString("StartDate");
+			String city = resultSet.getString("City");
+			String country = resultSet.getString("Country");
+			String address = resultSet.getString("Address");
+			String department = resultSet.getString("Department");
+			int basicPay = resultSet.getInt("BasicPay");
+			int deductions = resultSet.getInt("Deductions");
+			int taxAblePay = resultSet.getInt("TaxablePay");
+			int tax = resultSet.getInt("Tax");
+			
+			System.out.println("employeeID=" + ", employeeName=" + employeeName
+				+ ", netPay=" + netPay + ", startDate=" + startDate + ", city=" + city + ", country=" + country
+				+ ", address=" + address + ", department=" + department + ", basicPay="
+				+ basicPay + ", deductions=" + deductions + ", taxAblePay=" + taxAblePay + ", tax=" + tax);
+			
+		}
+		
+//		PreparedStatement statement = connection.prepareStatement(query);
+//		statement.setInt(1, employeePojo.getNetPay());
+//		statement.setString(2, employeePojo.getEmployeeName());
+		//int res = statement.executeUpdate();
+		//System.out.println(res+" :: records inserted");
 		
 		statement.close();
 		connection.close();
