c2b281d618c1d44066de8426c2dabd220a5a046e
dpp17 <durga123q.com>
2023-03-06 04:43:38 +0530

    Added:payroll services jdbc

--- src/payrollservices/connections/PayrollServicesJDBC.java
+++ src/payrollservices/connections/PayrollServicesJDBC.java
@@ -16,24 +16,13 @@ public class PayrollServicesJDBC {
 		final String userName = "root";
 		final String password = "Lambop@12345";
 		Connection connection = DriverManager.getConnection(url,userName,password);
-		String query = "insert into employee_payroll(EmployeeName, NetPay, StartDate, City, Country, Address, Department, BasicPay, Deductions, TaxablePay, Tax) values"
-				+ "(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+		String query = "update employee_payroll set NetPay = ? where EmployeeName = ?;";
 		
 		//Statement statement = connection.createStatement();
 		PreparedStatement statement = connection.prepareStatement(query);
-		statement.setInt(2, employeePojo.getNetPay());
-//		statement.setLong(7, employeePojo.getPhoneNumber());
-		statement.setInt(8, employeePojo.getBasicPay());
-		statement.setInt(10, employeePojo.getTaxAblePay());
-		statement.setInt(9, employeePojo.getDeductions());
-		statement.setInt(11, employeePojo.getTax());
-		statement.setString(1, employeePojo.getEmployeeName());
-//		statement.set(2, employeePojo.getGender());
-		statement.setString(3, employeePojo.getStartDate());
-		statement.setString(7, employeePojo.getDepartment());
-		statement.setString(5, employeePojo.getCountry());
-		statement.setString(4, employeePojo.getCity());
-		statement.setString(6, employeePojo.getAddress());
+		statement.setInt(1, employeePojo.getNetPay());
+		statement.setString(2, employeePojo.getEmployeeName());
+		
 
 		int res = statement.executeUpdate();
 		System.out.println(res+" :: records inserted");
--- src/payrollservices/controller/EmployeePayroll.java
+++ src/payrollservices/controller/EmployeePayroll.java
@@ -14,41 +14,41 @@ public class EmployeePayroll {
 		System.out.println("Enter Employee Name");
 		employeePojo.setEmployeeName(userInput.next());
 		
-		System.out.println("Enter gender");
-		employeePojo.setGender(userInput.next().charAt(0));
+//		System.out.println("Enter gender");
+//		employeePojo.setGender(userInput.next().charAt(0));
 		
 		System.out.println("Enter netPay");
 		employeePojo.setNetPay(userInput.nextInt());
-		
-		System.out.println("Enter startDate");
-		employeePojo.setStartDate(userInput.next());
-		
-		System.out.println("Enter city");
-		employeePojo.setCity(userInput.next());
-		
-		System.out.println("Enter country");
-		employeePojo.setCountry(userInput.next());
-		
-		System.out.println("Enter phoneNumber");
-		employeePojo.setPhoneNumber(userInput.nextLong());
-		
-		System.out.println("Enter department");
-		employeePojo.setDepartment(userInput.next());
-		
-		System.out.println("Enter basicPay");
-		employeePojo.setBasicPay(userInput.nextInt());
-		
-		System.out.println("Enter deductions");
-		employeePojo.setDeductions(userInput.nextInt());
-		
-		System.out.println("Enter taxAblePay");
-		employeePojo.setTaxAblePay(userInput.nextInt());
-		
-		System.out.println("Enter tax");
-		employeePojo.setTax(userInput.nextInt());
-		
-		System.out.println("Enter address");
-		employeePojo.setAddress(userInput.next());
+//		
+//		System.out.println("Enter startDate");
+//		employeePojo.setStartDate(userInput.next());
+//		
+//		System.out.println("Enter city");
+//		employeePojo.setCity(userInput.next());
+//		
+//		System.out.println("Enter country");
+//		employeePojo.setCountry(userInput.next());
+//		
+//		System.out.println("Enter phoneNumber");
+//		employeePojo.setPhoneNumber(userInput.nextLong());
+//		
+//		System.out.println("Enter department");
+//		employeePojo.setDepartment(userInput.next());
+//		
+//		System.out.println("Enter basicPay");
+//		employeePojo.setBasicPay(userInput.nextInt());
+//		
+//		System.out.println("Enter deductions");
+//		employeePojo.setDeductions(userInput.nextInt());
+//		
+//		System.out.println("Enter taxAblePay");
+//		employeePojo.setTaxAblePay(userInput.nextInt());
+//		
+//		System.out.println("Enter tax");
+//		employeePojo.setTax(userInput.nextInt());
+//		
+//		System.out.println("Enter address");
+//		employeePojo.setAddress(userInput.next());
 		
 		return employeePojo;
 	}
